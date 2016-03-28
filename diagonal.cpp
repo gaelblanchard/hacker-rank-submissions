91930db2a2dd9151cbee6f87d1bb4191369096f6
gaelblanchard <gaelblanchard@live.com>
2016-03-27 23:28:11 -0400

    Added files via upload

---
+++ diagonal.cpp
@@ -0,0 +1,45 @@
+//
+//  diagonal_sum.cpp
+//  
+//
+//  Created by new on 3/27/16.
+//
+//
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    int primarydiagsum=0;
+    int secondarydiagsum=0;
+    int finaldiff=0;
+    cin >> n;
+    vector< vector<int> > a(n,vector<int>(n));
+    for(int a_i = 0;a_i < n;a_i++){
+        for(int a_j = 0;a_j < n;a_j++){
+            cin >> a[a_i][a_j];
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        primarydiagsum=primarydiagsum+a[i][i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        secondarydiagsum=secondarydiagsum+a[i][n-(i+1)];
+    }
+    finaldiff=primarydiagsum-secondarydiagsum;
+    if(finaldiff<0)
+    {
+        finaldiff=finaldiff*-1;
+    }
+    cout<<finaldiff<<endl;
+    
+    return 0;
+}
+
---
+++ plusminus.cpp
@@ -0,0 +1,54 @@
+//
+//  plusminus.cpp
+//  
+//
+//  Created by new on 3/27/16.
+//
+//
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+    int n;//to allow fraction result
+    double positiveCounter=0.0;
+    double negativeCounter=0.0;
+    double zeroCounter=0.0;
+    long double fractionOutput;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+        cin >> arr[arr_i];
+    }
+    //counter for positive, negative and zero numbers
+    for(int i=0; i < n;i++)
+    {
+        if (arr[i]>0)
+        {
+            positiveCounter = positiveCounter + 1;
+        }
+        if (arr[i]<0)
+        {
+            negativeCounter= negativeCounter + 1;
+        }
+        if (arr[i]==0)
+        {
+            zeroCounter= zeroCounter + 1;
+        }
+    }
+    fractionOutput = positiveCounter/n;
+    cout << std::fixed << fractionOutput << std::setprecision(6)<<endl;
+    fractionOutput=negativeCounter/n;
+    cout << std::fixed << fractionOutput << std::setprecision(6)<<endl;
+    fractionOutput=zeroCounter/n;
+    cout << std::fixed << fractionOutput << std::setprecision(6)<< endl;
+    
+    return 0;
+}
+
